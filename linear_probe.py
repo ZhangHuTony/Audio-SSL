341334760ebedfab09edfa4e4ab8d6be90114122
Zhang-2023 <zhang66.t66@gmail.com>
2026-05-28 14:37:44 +0800

    Update linear_probe.py

--- linear_probe.py
+++ linear_probe.py
@@ -208,7 +208,7 @@ def main(args):
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--model", type=str, default="mae", choices=["mae", "mae_sota", "jepa"])
+    p.add_argument("--model", type=str, default="mae", choices=["mae", "mae_sota", "jepa", "cjepa"])
     p.add_argument("--data_dir", type=str, default="./data")
     p.add_argument("--batch_size", type=int, default=256)
     p.add_argument("--num_workers", type=int, default=4)
